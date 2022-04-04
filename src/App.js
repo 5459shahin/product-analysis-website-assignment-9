0ea17aa8ac8d296b24825766abd306bdb874bd23
545n shahin <mohammadshahinkhan1992@gmail.com>
2022-04-04 22:44:05 +0800

    add navbar and responsive

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './component/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-5xl'>assignment</h1>
+      {/* <Header></Header> */}
+      <Header></Header>
+        <h1 className='text-6xl'>  thi is a main component</h1>
+        
     </div>
   );
 }
